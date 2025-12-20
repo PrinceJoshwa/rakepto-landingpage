09a823f188aa34afb3d47ea8177eb4444f7d2534
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-12-20 20:28:26 +0530

    Update: Blog Page

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -799,7 +799,7 @@ const BlogCard = ({ post, index, isFavorite = false, onToggleFavorite }) => {
             }}
             src={imageUrl || "/placeholder.svg"}
             alt={post.title.rendered}
-            className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"
+            className="w-full h-full object-fill group-hover:scale-110 transition-transform duration-500"
           />
           <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
         </div>
